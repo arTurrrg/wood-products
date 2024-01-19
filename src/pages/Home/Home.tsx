028192699497f18a81cecc6cd2b46d05e72ef321
arTurrrg <arturgarstea@gmail.com>
2024-01-19 20:28:42 +0200

    updated line between text and photos

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,22 +10,21 @@ export default function Home() {
         className="brightness-50 max-h-screen absolute top-0 left-0 select-none z-[-1]"
       />
       <div className="rounded-3xl rounded-r-none bg-[#1E0C06] p-10 ">
-        <div className="text-6xl text-[#F5F5F5] leading-tight">
-          <p>SOLID</p>
-          <p>WOOD</p>
-          <p>PRODUCTS</p>
+        <div className="border-r-[1px] border-[#F5F5F5] pr-10 pb-14">
+          <div className="text-6xl text-[#F5F5F5] leading-tight">
+            <p>SOLID</p>
+            <p>WOOD</p>
+            <p>PRODUCTS</p>
+          </div>
+          <div className="relative left-2 top-5">
+            <p className="text-2xl text-[#FFDBBB]">
+              Oak, beech, ash from <br />
+              <span className="font-semibold">1700 CZK</span> per m3
+            </p>
+          </div>
         </div>
-        <div className="relative left-2 top-5">
-          <p className="text-2xl text-[#FFDBBB]">
-            Oak, beech, ash from <br />
-            <span className="font-semibold">1700 CZK</span> per m3
-          </p>
-        </div>
-        <div className="py-10 relative top-9">
+        <div className="py-11">
           <ButtonLink message="Order" />
-        </div>
-        <div className="flex h-[300px] p-2">
-          <div className="bg-[#F5F5F5] w-[1px]"></div>
         </div>
       </div>
     </div>
